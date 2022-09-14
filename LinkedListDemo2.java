415dbebdf865867ea59863869f0fb892a0a602d9
harshada1999 <pharshada800@gmail.com>
2022-09-14 12:43:56 +0530

    i added linked list

---
+++ LinkedListDemo2.java
@@ -0,0 +1,31 @@
+package com.velocity.collection;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class LinkedListDemo2 {
+	
+public static void main(String[] args) {
+		
+		LinkedList<Integer> primeNumberList = new LinkedList<Integer>();
+		primeNumberList.add(2);
+		primeNumberList.add(3);
+		primeNumberList.add(5);
+		primeNumberList.add(7);
+		
+		//using iterators
+		Iterator<Integer> itr = primeNumberList.iterator();
+		while(itr.hasNext()) {
+			System.out.println(itr.next());
+		}
+		
+		//using for-each loop
+		//for(Integer num : primeNumberList) {
+		//	System.out.println(num);
+		//}
+		
+		
+		
+	}
+
+}
